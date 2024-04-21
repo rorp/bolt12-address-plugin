f47e7c531b2f71c6ba6330c4b7d43dc85264485c
rorp <rorp@protonmail.com>
2024-04-20 18:59:00 -0700

    DNS over HTTPS support

--- src/main/scala/io/github/rorp/bolt12addressplugin/Bolt12Address.scala
+++ src/main/scala/io/github/rorp/bolt12addressplugin/Bolt12Address.scala
@@ -7,31 +7,11 @@ import org.minidns.record.TXT
 import scala.util.Try
 
 case class Bolt12Address(address: String) {
-  import io.github.rorp.bolt12addressplugin.Bolt12Address.Prefix
-
   def toDomainName: Try[String] = Try {
     address.split("@") match {
       case Array(user, host) => s"$user.user._bitcoin-payment.$host"
       case _ => throw new IllegalArgumentException("invalid BOLT12 address")
     }
-  }
-
-  def fetchOffer(): Try[Offer] = {
-    for {
-      domain <- toDomainName
-      dnsResponse <- Try(DnssecResolverApi.INSTANCE.resolve(domain, classOf[TXT]))
-      offerStr <- extractOfferString(dnsResponse)
-      offer <- Offer.decode(offerStr)
-    } yield offer
-  }
-
-  private def extractOfferString(resolverResult: ResolverResult[TXT]): Try[String] = Try {
-    val answers = resolverResult.getAnswers
-    if (answers.isEmpty) throw new RuntimeException("DNS response was empty")
-    if (answers.size > 1) throw new RuntimeException("too many DNS records")
-    val txt = answers.iterator().next().getText
-    if (!txt.startsWith(Prefix)) throw new RuntimeException(s"invalid DNS data: `$txt`")
-    txt.substring(Prefix.length)
   }
 }
 
